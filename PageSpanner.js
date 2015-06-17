094edc87702a8c69d7d2d2e93364d020165da1e4
nanoscopic <git@codechild.com>
2015-06-17 15:36:47 -0400

    Generate a table of contents. Fix finalization of pages so that splitting of pages works better. Alter page fill logic to work with new CSS and page methodology to completely fill printed pages. Make it so that graphs are not placed if they will not fit in remaining page space. Rerender all headers when part of the headers do not fit.

--- PageSpanner.js
+++ PageSpanner.js
@@ -3,9 +3,19 @@ PageSpanner.prototype = {
   curpage: 0,
   table: 0,
   bid: 0,
+  pagenum: 0,
+  curpage: 0,
   initialize: function( tables ) {
-    this.curpage = new Page();
+    this.pagenum = 0;
+    this.curpage = 0;
+    this.add_page();
     this.tables = tables;
+    this.tocBuilder = new TocBuilder( this );
+  },
+  add_page: function() {
+    if( this.curpage ) this.curpage.finalize();
+    this.pagenum++;
+    this.curpage = new Page( this.pagenum );
   },
   add_items: function( r ) {
     for( var i=0;i<r.length;i++ ) {
@@ -19,29 +29,51 @@ PageSpanner.prototype = {
         var type = v[0];
         if( type == 'table' ) {
           var tname = v[1];
-          this.add_table( this.tables[ tname ] );
+          var def = this.tables[ tname ];
+          if(def) this.add_table( def );
+          if(!def) console.log("Cannot find table named " + tname );
         }
         if( type == 'tableid' ) {
           var insertid = v[1];
           var tname = v[2];
-          this.add_table_in_id( insertid, this.tables[ tname ] );
+          var def = this.tables[ tname ];
+          if(def) this.add_table_in_id( insertid, def );
+          if(!def) console.log("Cannot find table named " + tname );
         }
         if( type == 'chart' ) {
           var cname = v[1];
           var tb = this.tables[ cname ];
-          var xys = this.findxys( tb );
-         if( xys ) this.add_graph( xys, tb['chart'] || 0, cname, 0 );
+          if(tb) {
+            var xys = this.findxys( tb );
+            if( xys ) this.add_graph( xys, tb['chart'] || 0, cname, 0 );
+          }
+          if(!tb) console.log("Cannot find table named " + cname );
         }
         if( type == 'chartid' ) {
           var insertid = v[1];
           var cname = v[2];
           var tb = this.tables[ cname ];
-          var xys = this.findxys( tb );
-          if( xys ) this.add_graph( xys, tb['chart'] || 0, cname, insertid );
+          if(tb) {
+            var xys = this.findxys( tb );
+            if( xys ) this.add_graph( xys, tb['chart'] || 0, cname, insertid );
+          }
+          if(!tb) console.log("Cannot find table named " + cname );
+        }
+        if( type == 'index' ) {
+          var level = v[1];
+          var name = v[2];
+          var tpl = v[3];
+          this.tocBuilder.addItem( this.pagenum, level, name, tpl );
+        }
+        if( type == 'toc' ) {
+          this.tocBuilder.addToc();
         }
       }
     }
-    this.curpage.finalize();
+    this.add_page();
+  },
+  fillToc: function() {
+    this.tocBuilder.renderToc();
   },
   findxys: function( tb ) {
     if( tb.xys ) return tb.xys;
@@ -65,8 +97,7 @@ PageSpanner.prototype = {
       var child = div.firstChild;
       this.curpage.append( child );
       if( this.curpage.is_full() ) {
-        this.curpage.finalize();
-        this.curpage = new Page();
+        this.add_page();
         this.curpage.append( child );
       }
     }
@@ -94,8 +125,7 @@ PageSpanner.prototype = {
     _append( this.curtbody, tr );
     if( this.curpage.is_full() ) {
       //tbody.removeChild( tr ); // in theory we can skip this
-      this.curpage.finalize();
-      this.curpage = new Page();
+      this.add_page();
       this.curtbody = this.add_empty_table();
       callback();
       _append( this.curtbody, tr );
@@ -164,6 +194,15 @@ PageSpanner.prototype = {
       type = inf.type;
       if( inf.options ) {
         var addops = inf.options;
+        var height = addops.height;
+        var remain = this.curpage.remaining_height();
+        if( remain < 0 || height > remain ) {
+          _del( div );
+          this.add_page();
+          if( !insertid ) this.add_graph( xys, inf, tbname, insertid );
+          return;
+        }
+        
         if( addops.labelInterpolationFnc ) {
           var lif;
           eval( 'lif = ' + addops.labelInterpolationFnc );
@@ -227,6 +266,61 @@ PageSpanner.prototype = {
     var bod = this.add_empty_table();
     this.curtbody = bod;
     return bod;
+  }
+};
+var TocBuilder = Class.create();
+TocBuilder.prototype = {
+  pageSpanner: 0,
+  items: 0,
+  id: 0,
+  initialize: function( pageSpanner ) {
+    this.pageSpanner = pageSpanner;
+    this.items = [];
+    this.cascade = new DomCascade();
+  },
+  addItem: function( pagenum, level, text, html ) {
+    var id = ++this.id;
+    this.pageSpanner.add_html( "<a name='toc" + id + "'>" + html + "</a>" );
+    this.items.push( { level: level, text: text, id: id, pagenum: pagenum } );
+  },
+  addToc: function() {
+    this.pageSpanner.add_html( "<div id='toc'></div>" );
+  },
+  renderToc: function() {
+    var toc = _getel('toc');
+    
+    var tb = _newtable('table');
+    _append( toc, tb.table );
+    for( var i=0;i<this.items.length;i++ ) {
+      var item = this.items[ i ];
+      var level = item.level;
+      
+      var res = this.cascade.flow( {
+        name: 'tr',
+        sub: [
+          { name: 'td',
+            sub: {
+              name: "a",
+              attr: {
+                href: '#toc' + item.id
+              },
+              sub: {
+                name: "text",
+                text: item.text
+              }
+            }
+          },
+          { name: 'td',
+            sub: {
+              name: 'text',
+              text: item.pagenum
+            }
+          }
+        ]
+      } );
+      
+      _append( tb.tbody, res.node );
+    }
   }
 };
 var TableSys = Class.create();
@@ -296,6 +390,7 @@ TableLevel.prototype = {
               if( above.def.header ) above.renderHeaders( above.def.header );
             }
           }
+          if( self.def.header ) self.renderHeaders( self.def.header );
       } );
     }
   },
@@ -307,31 +402,46 @@ TableLevel.prototype = {
     this.shownHeaders = [];
   },
   renderHeaders: function( headers ) {
+    var nofit = 0;
+    
+    var heads = [];
     for( var i in headers ) {
       var header = headers[ i ];
       var tr = _newel('tr');
       //var levelEmpty = (this.shownHeaders.length == 0);
       this.shownHeaders.push( tr );
+      heads.push (tr );
       tr.innerHTML = header;
       var self = this;
       //var doKill = ( i == 0 ) ? 1 : 0;
       
       this.sys.addTr( tr, function( levelEmpty ) {
+          nofit = 1;
+          /*
           var showLevel;
           if( self.levelId > 0 ) {
             var maxShow = self.levelId - 1;
-            /*for( var showLevel = maxShow; showLevel >= 0; showLevel-- ) {
+            /for( var showLevel = maxShow; showLevel >= 0; showLevel-- ) {
               var showLevel = maxShow;
               var above = self.sys.getLevel( showLevel );
               if( doKill ) { above.killHeaders(); doKill = 0; }
-            }*/
+            }/
             
             for( var showLevel = 0; showLevel <= maxShow; showLevel++ ) {
               var above = self.sys.getLevel( showLevel );
               if( above.def.header ) above.renderHeaders( above.def.header );
             }
-          }
+          }*/
       } );
+      
+      if( nofit ) break;
+    }
+    
+    if( nofit ) {
+      for( var i=0;i<heads.length;i++ ) {
+        _del( heads[i] );
+      }
+      this.renderHeaders( headers );
     }
   },
   renderGroups: function( groups ) {
@@ -340,7 +450,7 @@ TableLevel.prototype = {
       var sets = group.sets;
       for( var j in sets ) {
         var set = sets[j];
-        if( set.use_new_table ) this.sys.newTb();
+        if( group.use_new_table && j > 0 ) this.sys.newTb();
         var level = new TableLevel( set, this.spanner, this.sys, this.levelId + 1 );
         this.sys.pushLevel( level );
         level.render();
@@ -353,18 +463,28 @@ TableLevel.prototype = {
 var Page = Class.create();
 Page.prototype = {
   div: 0,
-  initialize: function() {
+  pagenum: 0,
+  baseHeight: 0,
+  initialize: function( pagenum ) {
     //var main = _getel('main');
+    this.pagenum = pagenum;
     var div = _newdiv('page');
     _append( document.body, div );
+    this.baseHeight = div.offsetHeight;
+    div.style.height = '500px';
+    div.style.minHeight = 0;
     this.div = div;
   },
   is_full: function() {
     var h = this.height();
-    return h > 1100;
+    return h > this.baseHeight;
+  },
+  remaining_height: function() {
+    var h = this.height();
+    return this.baseHeight - h;
   },
   height: function() {
-    return this.div.offsetHeight;
+    return this.div.scrollHeight;
   },
   append: function( x ) {
     return _append( this.div, x );
